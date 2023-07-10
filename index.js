d29b1d4849da5fbe7a03c4477a998f37f0637f3b
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-07-10 14:59:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+var num1 = 31
+var num2 = 2
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random()*100) + 1
+
+var num3 = 18
+var num4 = 7
+var mod = num3 % num4
+
+var numbers = [5, 10, 20, 15]
+var max = Math.max(...numbers)
